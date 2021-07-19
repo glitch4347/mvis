43b6fa79c697bf8493b0aa568bfed98f0ec7bbd9
Coin de Gamma <cg@corpglory.com>
2021-07-19 12:03:33 +0200

    some commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,21 @@ use macroquad::prelude::*;
 
 
 struct Vis {
-    pos: Vec2
+    pos: Vec2,
+    bars: Vec<Bar>
+}
+
+impl Vis {
+    fn new() -> Vis {
+        Vis{
+            pos: Vec2::new(screen_width() / 2.,  screen_height() / 2.),
+            bars: Vec::new()
+        }
+    }
+
+    fn render(&self) {
+        draw_circle(self.pos.x, self.pos.y, 10., BLACK);
+    }
 }
 
 struct Bar {
@@ -20,16 +34,13 @@ async fn main() {
     //     println!("{}", argument);
     // }
 
-    let vis = Vis {
-        pos: Vec2::new(screen_width() / 2.,  screen_height() / 2.) 
-    };
-
+    let vis = Vis::new();
 
 
     loop {
         clear_background(LIGHTGRAY);
 
-        draw_circle(vis.pos.x, vis.pos.y, 2., BLACK);
+        vis.render();
 
         next_frame().await
     }
