a05d7b2126651ce70345e5377d72eb327bf5da14
Coin de Gamma <cg@corpglory.com>
2021-07-20 12:34:22 +0200

    some commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::f32;
-use std::env;
+use std::{env, ops::Mul};
 
 use macroquad::prelude::*;
 
@@ -18,13 +18,12 @@ impl Vis {
         let mut bars = Vec::<Bar>::new();
 
         let pos = Vec2::new(screen_width() / 2.,  screen_height() / 2.);
-        let radius = screen_width().min(screen_height()) / 2.;
-
-        
+        let radius = screen_width().min(screen_height()) / 3.;
 
         for i in 0..bars_count {
-            let rotation = i as f32 * 20.;
-            bars.push(Bar::new(pos, rotation, radius))
+            let length = 180. / bars_count as f32;
+            let rotation = i as f32 * length;
+            bars.push(Bar::new(pos, rotation.to_radians(), radius))
         }
         Vis{
             pos,
@@ -36,8 +35,10 @@ impl Vis {
 
     fn render(&self) {
         draw_circle(self.pos.x, self.pos.y, 10., BLACK);
+        let mut pulse = 1.;
         for b in &self.bars {
-            b.render();
+            b.render(pulse);
+            pulse += 1.5;
         }
     }
 }
@@ -52,11 +53,17 @@ impl Bar {
     fn new(origin: Vec2, rotation: f32, radius: f32) -> Bar {
         Bar { origin, rotation, radius }
     }
-    fn render(&self) {
-        let posx = self.radius * self.rotation.cos() + self.origin.x;
-        let posy = self.radius * self.rotation.sin() + self.origin.y;
-        draw_circle(posx, posy, 10., BLACK);
+    fn render(&self, pulse: f32) {
+        let x0 = self.radius * self.rotation.cos() + self.origin.x;
+        let y0 = self.radius * self.rotation.sin() + self.origin.y;
+        draw_circle(x0, y0, 5., BLACK);
 
+        let x1 = x0 + self.rotation.cos() * pulse / 2.;
+        let y1 = y0 + self.rotation.sin() * pulse / 2.;
+        let x2 = x0 + self.rotation.cos() * -pulse / 2.;
+        let y2 = y0 + self.rotation.sin() * -pulse / 2.;
+
+        draw_line(x1, y1, x2, y2, 2., GREEN);
     }
 }
 
@@ -68,7 +75,7 @@ async fn main() {
     //     println!("{}", argument);
     // }
 
-    let vis = Vis::new(5);
+    let vis = Vis::new(35);
 
 
     loop {
